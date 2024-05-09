6497f89d53f7ca633a3c1438c284f1eb7cbbe7b4
rafael <rafael@rafael>
2024-05-09 18:54:41 -0300

    update algorithms_comparative.py to include clonalg

--- libs/algorithms_comparative.py
+++ libs/algorithms_comparative.py
@@ -1,3 +1,4 @@
+from libs.clonalg import ClonalG
 from libs.differencial_evolution import DifferencialEvolution
 from libs.genetic_algorithm_binary import GeneticAlgorithmBinary
 import numpy as np
@@ -37,7 +38,7 @@ if __name__ == "__main__":
         GA = GeneticAlgorithmBinary(
             population_size=880,
             generations_size=30,
-            cost_function=shubert_cost,
+            cost_function=levy_cost,
             selection_method="tournament",
             mutation_rate=0.05,
             crossover_rate=0.7,
@@ -48,7 +49,7 @@ if __name__ == "__main__":
         mean_fitness_ga.append(mean_f)
     final = time.time()
     print("-"*100)
-    print(f"TEMPO DE 30 EXECUÇÕES DO GA: {final - initial}")
+    print(f"TEMPO DE 30 EXECUÇÕES DO GA: {round(final - initial, 2)} seg")
     print("-" * 100)
 
     boxplot_data = np.hstack((boxplot_data, np.array(best_fitness_ga).reshape(-1, 1)))
@@ -59,7 +60,7 @@ if __name__ == "__main__":
 
     initial = time.time()
     for i in range(30):
-        pso = ParticleSwarmOptimization(cost_function=shubert_cost,
+        pso = ParticleSwarmOptimization(cost_function=levy_cost,
                                         interations_size=30,
                                         size_particles=440,
                                         c1=2, c2=2,
@@ -71,7 +72,7 @@ if __name__ == "__main__":
         mean_fitness_pso.append(mean_fitness)
     final = time.time()
     print("-" * 100)
-    print(f"TEMPO DE 30 EXECUÇÕES DO PSO: {final - initial}")
+    print(f"TEMPO DE 30 EXECUÇÕES DO PSO: {round(final - initial, 2)} seg")
     print("-" * 100)
 
     boxplot_data = np.hstack((boxplot_data, np.array(best_fitness_pso).reshape(-1, 1)))
@@ -85,7 +86,7 @@ if __name__ == "__main__":
         DE = DifferencialEvolution(population_size=440,
                                    interations_size=30,
                                    crossover_rate=0.9,
-                                   cost_function=shubert_cost,
+                                   cost_function=levy_cost,
                                    with_oposition_learning=False,
                                    F=2)
 
@@ -95,25 +96,50 @@ if __name__ == "__main__":
         mean_fitness_de.append(mean_fitness)
     final = time.time()
     print("-" * 100)
-    print(f"TEMPO DE 30 EXECUÇÕES DO DE: {final - initial}")
+    print(f"TEMPO DE 30 EXECUÇÕES DO DE: {round(final - initial, 2)} seg")
     print("-" * 100)
 
     boxplot_data = np.hstack((boxplot_data, np.array(best_fitness_de).reshape(-1, 1)))
     boxplot_data_mean = np.hstack((boxplot_data_mean, np.array(mean_fitness_de).reshape(-1, 1)))
 
+    best_fitness_clonalg = []
+    mean_fitness_clonalg = []
+
+    initial = time.time()
+    for i in range(30):
+        clonalg = ClonalG(population_size=440,
+                          cloning_rate=0.5,
+                          mutation_range=0.9,
+                          interations_size=30,
+                          selection_rate=0.9,
+                          cost_function=levy_cost
+                          )
+
+        best_fitness, mean_fitness = clonalg.execute()
+
+        best_fitness_clonalg.append(best_fitness)
+        mean_fitness_clonalg.append(mean_fitness)
+    final = time.time()
+    print("-" * 100)
+    print(f"TEMPO DE 30 EXECUÇÕES DO CLONALG: {round(final - initial, 2)} seg")
+    print("-" * 100)
+
+    boxplot_data = np.hstack((boxplot_data, np.array(best_fitness_clonalg).reshape(-1, 1)))
+    boxplot_data_mean = np.hstack((boxplot_data_mean, np.array(mean_fitness_clonalg).reshape(-1, 1)))
+
     plt.figure(figsize=(10, 5))
-    plt.title(f"Mean Fitness")
-    df2 = pd.DataFrame(boxplot_data_mean, columns=['GA', 'PSO', 'DE'])
+    plt.title(f"Mean Fitness (levy_cost)")
+    df2 = pd.DataFrame(boxplot_data_mean, columns=['GA', 'PSO', 'DE', 'CLONALG'])
     boxplot2 = df2.boxplot()
     fig2 = boxplot2.get_figure()
 
-    fig2.savefig('boxplot_mean_shubert_cost_comparative_ga_pso_de.png')
+    fig2.savefig('boxplot_mean_levy_cost_comparative_ga_pso_de_clonalg.png')
 
     plt.figure(figsize=(10, 5))
-    plt.title(f"Best Fitness")
-    df = pd.DataFrame(boxplot_data, columns=['GA', 'PSO', 'DE'])
+    plt.title(f"Best Fitness (levy_cost)")
+    df = pd.DataFrame(boxplot_data, columns=['GA', 'PSO', 'DE', 'CLONALG'])
     boxplot = df.boxplot()
     fig = boxplot.get_figure()
 
-    fig.savefig('boxplot_best_shubert_cost_comparative_ga_pso_de.png')
+    fig.savefig('boxplot_best_levy_cost_comparative_ga_pso_de_clonalg.png')
 
