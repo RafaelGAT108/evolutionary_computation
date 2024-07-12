a22cb065a2f4936c2d3013ca8947c7a791c23728
rafael.avila <rafael.avila@enacom.com.br>
2024-07-11 21:39:24 -0300

    range for min and max value os X1 and X2 included in GWO

--- libs/gray_wolf_optimization.py
+++ libs/gray_wolf_optimization.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass, field
+from typing import Union
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from functions import *
 import time
 import multiprocessing
+
+
 @dataclass
 class Wolf:
     position: np.ndarray
@@ -17,11 +20,20 @@ class GrayWolfOptimization:
     population: list[Wolf] = field(init=False)
     population_size: int
     interations_size: int
+    min_range: Union[float, int, list]
+    max_range: Union[float, int, list]
 
     def __post_init__(self):
-        self.population = [Wolf(position=np.random.randn(2))
-                           for _ in range(self.population_size)]
+        # self.population = [Wolf(position=np.random.randn(2))
+        #                    for _ in range(self.population_size)]
+        if isinstance(self.max_range, Union[float, int]) and isinstance(self.min_range, Union[float, int]):
+            self.population = [Wolf(position=np.random.uniform(self.min_range, self.max_range, 2))
+                               for _ in range(self.population_size)]
 
+        elif type(self.max_range) is list and type(self.min_range) is list:
+            self.population = [Wolf(position=np.array([float(np.random.uniform(self.min_range[0], self.min_range[1], 1)[0]),
+                                                       float(np.random.uniform(self.max_range[0], self.max_range[1], 1)[0])]))
+                               for _ in range(self.population_size)]
         self.alpha = self.beta = self.delta = None
 
     def calculate_x(self, a, top_wolf, wolf):
@@ -64,7 +76,6 @@ def process_step(equation):
     initial = time.time()
     best_individuals = []
     for _ in range(30):
-
         gray = GrayWolfOptimization(cost_function=equation,
                                     population_size=100,
                                     interations_size=1000)
@@ -72,7 +83,7 @@ def process_step(equation):
         alpha = gray.execute()
         best_individuals.append(alpha)
 
-    best_individuals = sorted(best_individuals, key= lambda x: x.fitness)
+    best_individuals = sorted(best_individuals, key=lambda x: x.fitness)
 
     final = time.time()
     print("-" * 100)
@@ -89,27 +100,48 @@ def process_step(equation):
 
 if __name__ == "__main__":
     functions = {
-        'goldstein_price_cost': goldstein_price_cost,
-        'branin_cost': branin_cost,
-        'beale_cost': beale_cost,
-        'michalewicz_cost': michalewicz_cost,
-        'easom_cost': easom_cost,
-        'shubert_cost': shubert_cost,
-        'schwefel_cost': schwefel_cost,
-        'schaffer_4_cost': schaffer_4_cost,
-        'schaffer_2_cost': schaffer_2_cost,
-        'rastrigin_function': rastrigin_function,
-        'levy_13_cost': levy_13_cost,
-        'levy_cost': levy_cost,
-        'langermann_cost': langermann_cost,
-        'holder_table_cost': holder_table_cost,
-        'griewank_cost': griewank_cost,
-        'eggholder_cost': eggholder_cost,
-        'drop_cost': drop_cost,
-        'cross_cost': cross_cost,
-        'bukin_cost': bukin_cost,
-        'ackley_cost': ackley_cost,
-        'jong_cost': jong_cost
+        # 'goldstein_price_cost': dict(func=goldstein_price_cost,
+        #                              range=[-2, 2]),
+        # 'branin_cost': dict(func=branin_cost,
+        #                     range=[[-5, 10], [0, 15]]),
+        # 'beale_cost': dict(func=beale_cost,
+        #                    range=[-4.5, 4.5]),
+        # 'michalewicz_cost': dict(func=michalewicz_cost,
+        #                          range=[0, np.pi]),
+        # 'easom_cost': dict(func=easom_cost,
+        #                    range=[-100, 100]),
+        # 'shubert_cost': dict(func=shubert_cost,
+        #                      range=[-10, 10]),
+        # 'schwefel_cost': dict(func=schwefel_cost,
+        #                       range=[-500, 500]),
+        # 'schaffer_4_cost': dict(func=schaffer_4_cost,
+        #                         range=[-100, 100]),
+        # 'schaffer_2_cost': dict(func=schaffer_2_cost,
+        #                         range=[-100, 100]),
+        # 'rastrigin_function': dict(func=rastrigin_function,
+        #                            range=[-5.12, 5.12]),
+        # 'levy_13_cost': dict(func=levy_13_cost,
+        #                      range=[-10, 10]),
+        # 'levy_cost': dict(func=levy_cost,
+        #                   range=[-10, 10]),
+        # 'langermann_cost': dict(func=langermann_cost,
+        #                         range=[0, 10]),
+        # 'holder_table_cost': dict(func=holder_table_cost,
+        #                           range=[-10, 10]),
+        # 'griewank_cost': dict(func=griewank_cost,
+        #                       range=[-600, 600]),
+        'eggholder_cost': dict(func=eggholder_cost,
+                               range=[-512, 512]),
+        'drop_cost': dict(func=drop_cost,
+                          range=[-5.12, 5.12]),
+        'cross_cost': dict(func=cross_cost,
+                           range=[-10, 10]),
+        'bukin_cost': dict(func=bukin_cost,
+                           range=[[-15, -5], [-3, 3]]),
+        'ackley_cost': dict(func=ackley_cost,
+                            range=[-32.768, 32.768]),
+        'jong_cost': dict(func=jong_cost,
+                          range=[-65.536, 65.536])
     }
     list_functions = functions.values()
 
@@ -126,9 +158,11 @@ if __name__ == "__main__":
 
         best_individuals = []
         for _ in range(30):
-            gray = GrayWolfOptimization(cost_function=equation,
+            gray = GrayWolfOptimization(cost_function=equation['func'],
                                         population_size=100,
-                                        interations_size=1000)
+                                        interations_size=1000,
+                                        max_range=equation['range'][1],
+                                        min_range=equation['range'][0])
 
             alpha = gray.execute()
             best_individuals.append(alpha)
